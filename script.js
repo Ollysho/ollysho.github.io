d8e435592f7494fa4d51f23c9b0b19c0f9dd2dbc
Olly Sholotan <ollysho@gmail.com>
2016-08-08 02:28:21 -0500

    Added MV to projects page and others
    Quite a few design changes, front page is updated. Added more stuff to
    the projects page and general styling is different

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     
     var nav = $('#nav')
     $(window).scroll(function() {
-        if ( $(this).scrollTop() > 200 ) {
+        if ( $(this).scrollTop() > 175 ) {
             nav.addClass('nav-fixed');
         } else {
             nav.removeClass('nav-fixed');
