6b3497162e9097c3d1aaec607c8f1e10fd952e5a
Olly Sholotan <ollysho@gmail.com>
2016-08-19 13:51:41 -0500

    Added Mobile Menu Button

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
     
-    var nav = $('#nav')
+    //Nav Scroll Script
+    var nav = $('#nav');
     $(window).scroll(function() {
-        if ( $(this).scrollTop() > 175 ) {
+        if ( $(this).scrollTop() > 175 && $(this).width() > 500 ) {
             nav.addClass('nav-fixed');
         } else {
             nav.removeClass('nav-fixed');
         }
     });
+    
+    
+    //Mobile Menu Button Script
+    var menu = $('#menu-btn');
+    menu.click(function() {
+        if (nav.css('display') === 'none') {
+            nav.css('display', 'block');
+        } else {
+            nav.css('display', 'none');
+        }
+    });
+    
+    if ( $(window).width() > 500) {
+        nav.css('display', 'inline');
+    }
 });
